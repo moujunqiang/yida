d8ea3cf33cb9b07d80010db50f257019c40dd54b
moujunqiang <705174710@qq.com>
2020-12-22 18:09:29 +0800

    123

--- app/src/main/java/com/android/yida/MainActivity.java
+++ app/src/main/java/com/android/yida/MainActivity.java
@@ -1,14 +1,60 @@
 package com.android.yida;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.yida.adapter.BaseFragmentAdapter;
+import com.android.yida.fragment.MeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private ViewPager mViewPager;
+    private BottomNavigationView mBottomNavigationView;
+
+    private BaseFragmentAdapter<Fragment> mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewPager = findViewById(R.id.vp_home_pager);
+        mBottomNavigationView = findViewById(R.id.bv_home_navigation);
+
+        // 不使用图标默认变色
+        mBottomNavigationView.setItemIconTintList(null);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+        initData();
     }
+
+    protected void initData() {
+        mPagerAdapter = new BaseFragmentAdapter<>(this);
+        mPagerAdapter.addFragment(MeFragment.newInstance());
+        mPagerAdapter.addFragment(MeFragment.newInstance());
+        // 设置成懒加载模式
+        mPagerAdapter.setLazyMode(true);
+        mViewPager.setAdapter(mPagerAdapter);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                mViewPager.setCurrentItem(0);
+                return true;
+            case R.id.menu_me:
+                mViewPager.setCurrentItem(1);
+                return true;
+            default:
+                break;
+        }
+        return false;
+    }
+
+
 }
---
+++ app/src/main/java/com/android/yida/fragment/MeFragment.java
@@ -0,0 +1,39 @@
+package com.android.yida.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.yida.R;
+
+public class MeFragment extends Fragment {
+
+    private View inflate;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        inflate = inflater.inflate(R.layout.fragment_me, container, false);
+        initView();
+        return inflate;
+    }
+
+    public void initView() {
+
+    }
+
+    public static MeFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        MeFragment fragment = new MeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
